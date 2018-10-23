2a0504b6677ef8ee455d94f3b09bba68b8ebc2ca
sumwin <sumwin@gmail.com>
2018-10-23 23:06:23 +0800

    Factory Function

--- index.js
+++ index.js
@@ -368,16 +368,20 @@
 
 //Object-Programming (OOP)
 
-const circle = {
-    radius: 1,
-    location:{
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw: function(){
-        console.log('draw');        
-    }
-};
 
-circle.draw();  //Method
+
+//Factory Function
+function createCircle(radius, location) {
+    return {
+        radius,
+        draw(){
+            console.log('draw');        
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
